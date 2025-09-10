4cd3ee5836bb0a5711766466a3b801c3fa9c01ed
Andrii Svorak <Andrew.Svorak@gmail.com>
2025-09-10 22:43:24 +0200

    Fix build and publish changes

--- src/use-combine-latest.fn.ts
+++ src/use-combine-latest.fn.ts
@@ -1,14 +1,15 @@
 import { computed, Signal } from '@angular/core';
 import { SignalValue } from './models/signal-value.type';
 
-export function useCombineLatest<Sources extends readonly Signal<unknown>[]>(
-  sources: Sources,
-): Signal<Sources | undefined> {
-  return computed(() => {
-    const values = sources.map((source) => source()) as {
-      [K in keyof Sources]: SignalValue<Sources[K]>;
-    };
+export type SourceValues<Sources extends readonly Signal<unknown>[]> = {
+  [K in keyof Sources]: SignalValue<Sources[K]>;
+};
 
+export function useCombineLatest<Sources extends readonly Signal<unknown>[]>(
+  sources: Sources
+): Signal<SourceValues<Sources> | undefined> {
+  return computed(() => {
+    const values = sources.map((source) => source()) as SourceValues<Sources>;
     return values.every((value) => value !== undefined) ? values : undefined;
   });
 }
